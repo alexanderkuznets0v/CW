730ba3827839b99aac85a6421593a44b8ee35097
alexander <astartes.imp@gmail.com>
2015-03-02 16:35:31 +0300

    forth python commit

--- dpp.c
+++ dpp.c
@@ -1,6 +1,26 @@
+#include <Python.h>
 #include <stdio.h>
 #include "dpp.h"
 
-void hello() {
+static PyObject *
+PyObject * hello(PyObject *self, PyObject *args, PyObject *kwargs) {
 	printf("Hello!\n");
+	Py_RETURN_NONE;
 }
+
+static PyMethodDef methods[] = {
+	{"hellow", hello, METHOd_VARARGS|METH_KEYWORDS, "Say hello!"),
+	{NULL, NULL, 0, NULL}
+};
+static PyModuleDef module ={
+	PyModuleDef+HEAD_INIT,
+	"b",
+	NULL,
+	-1,
+	methods
+};
+
+PyMODINIT_FUNC
+PyInit_b(void) {
+	return PyModule_Create(&module);
+};
